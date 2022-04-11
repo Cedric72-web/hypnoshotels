d216a8e90b2a541f0defe69249674236248f857d
VESQUE Cédric <vesque.cedric@bbox.fr>
2022-04-11 18:55:32 +0200

    Refactoring index.html -> PHP & Add footer.php

---
+++ includes/accueil.php
@@ -0,0 +1,8 @@
+<section id="accueil">
+    <div class="image-accueil">
+        <img src="images/image-accueil.jpg" alt="Vue Tour Eiffel">
+    </div>
+    <div class="description">
+        <p>Fondé en 2004, le groupe hôtelier Hypnos, compte 7 établissements dans les 4 coins de la France.<br>Destination idéale pour les couples en quête d'un séjour romantique, chaque hôtel est équipé de suite avec spa privatif, et propose des services hauts de gamme.<br>L'atmosphère chic-romantique de nos établissements vous permettra de cultiver la flamme qui anime votre couple. </p>
+    </div>
+</section>
---
+++ includes/contact.php
@@ -0,0 +1,3 @@
+<section id="contact">
+    <h2>Nous Contacter</h2>
+</section>
